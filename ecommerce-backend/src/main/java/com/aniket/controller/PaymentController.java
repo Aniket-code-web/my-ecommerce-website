7f70d46449ba5865279d3624aa2ebf341cb66ae3
Aniket code web <aniketmuni923@gmail.com>
2025-12-20 14:58:09 +0530

    refixed paymentcontroller

--- ecommerce-backend/src/main/java/com/aniket/controller/PaymentController.java
+++ ecommerce-backend/src/main/java/com/aniket/controller/PaymentController.java
@@ -66,7 +66,7 @@ public class PaymentController {
 
             paymentLinkRequest.put(
                     "callback_url",
-                    "https://your-frontend-domain.vercel.app" + orderId + "?order_id=" + orderId
+                    "https://aniketmuni-ecommerce.vercel.app/" + orderId + "?order_id=" + orderId
             );
             paymentLinkRequest.put("callback_method", "get");
 
